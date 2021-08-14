8e7281b01922432bc96c5ab41cb3e33882beaad9
Gattesaivamsi <78359124+Gattesaivamsi@users.noreply.github.com>
2021-08-14 15:32:59 +0530

    Update QUESTION1.java

--- QUESTION1.java
+++ QUESTION1.java
@@ -2,16 +2,16 @@ import java.util.*;
 public class QUESTION1 {
 	public static void main(String args[]) {
 	    Scanner sc = new Scanner(System.in);
-	    int i,n=25;
+	    int m,n=25;
 	    int Array[] = new int[n];
 	    System.out.println("Enter 25 elements of array : ");
-		for(i=0;i<n;i++){
-			Array[i]=sc.nextInt();	
+		for(m=0;m<n;m++){
+			Array[m]=sc.nextInt();	
 		}
 	    int max = Array[0];
-	    for (i = 1; i < n; i++){
-	        if (Array[i] > max)
-	            max = Array[i];
+	    for (m = 1; m < n; m++){
+	        if (Array[m] > max)
+	            max = Array[m];
 	       }
 	    System.out.println(max);
 	}
